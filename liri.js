4dd5e2f82f41ef74544bc814d6811ddb24375b96
mitburr <mitburr@gmail.com>
2019-02-21 23:49:52 -0600

    final update

--- liri.js
+++ liri.js
@@ -1,6 +1,5 @@
-
 require("dotenv").config();
-var fs = require("fs");
+var fs = require("file-system");
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
@@ -12,6 +11,20 @@ let operator = process.argv[2];
 let argument = process.argv.slice(3).join(" ");
 
 
+var content;
+fs.readFile('random.txt', "utf8", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+    processFile(content);
+});
+let variablesArray = "";
+function processFile(content) {
+    variablesArray = content.split("*")
+}
+
+
 let concertThis = function (artist) {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.BIT.apiKey)
         .then(function (response) {
@@ -19,7 +32,7 @@ let concertThis = function (artist) {
             let str = response.data[0].datetime;
             let day = str.split("T");
             //console logging the requested data
-            console.log(artist + "'s next show is at " + response.data[0].venue.name  + " in " +  response.data[0].venue.city + ","  + response.data[0].venue.region + " on " + moment(day[0]).format("MM/DD/YYYY")) ;
+            console.log(artist + "'s next show is at " + response.data[0].venue.name + " in " + response.data[0].venue.city + "," + response.data[0].venue.region + " on " + moment(day[0]).format("MM/DD/YYYY"));
         })
         .catch(function (error) {
             if (error.response) {
@@ -41,35 +54,27 @@ let concertThis = function (artist) {
         });
 }
 
-let spotifyThisSong = function(songName){
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
+let spotifyThisSong = function (songName) {
+    console.log(songName);
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-      console.log("the first result for you search query is " + data.tracks.items[0].name + " by " + data.tracks.items[0].album.artists[0].name + " from the album " + data.tracks.items[0].album.name +". It can be found at " + data.tracks.items[0].album.external_urls.spotify);
-      });
+        console.log(songName);
+        console.log("the first result for you search query is " + data.tracks.items[0].name + " by " + data.tracks.items[0].album.artists[0].name + " from the album " + data.tracks.items[0].album.name + ". It can be found at " + data.tracks.items[0].album.external_urls.spotify);
+    });
 }
-    
-let movieThis = function(movie){
-    if(!movie){movie = "Mr. Nobody"};
+
+let movieThis = function (movie) {
+    if (!movie) { movie = "Mr. Nobody" };
     movie.split(" ").join("+");
     axios.get("http://www.omdbapi.com/?apikey=" + keys.OMDB.apiKey + "&t=" + movie)
         .then(function (response) {
-            console.log("The movie " + response.data.Title + " and it was released on " + response.data.Released + 
-            ". \nIt has a rating of  " + response.data.Ratings[0].Value + " on IMDB and " + response.data.Ratings[1].Value + " fresh on Rotten Tomatoes \nIt was produced in " +
-            response.data.Country + " and it includes languages " + response.data.Language  + ". The plot of the film is: \n"  + 
-            response.data.Plot + " \n The film is acted by " + response.data.Actors + ".");
-
-
-    //    * Title of the movie.
-    //    * Year the movie came out.
-    //    * IMDB Rating of the movie.
-    //    * Rotten Tomatoes Rating of the movie.
-    //    * Country where the movie was produced.
-    //    * Language of the movie.
-    //    * Plot of the movie.
-    //    * Actors in the movie.
-        })
+            console.log("The movie " + response.data.Title + " and it was released on " + response.data.Released +
+                ". \nIt has a rating of  " + response.data.Ratings[0].Value + " on IMDB and " + response.data.Ratings[1].Value + " fresh on Rotten Tomatoes \nIt was produced in " +
+                response.data.Country + " and it includes languages " + response.data.Language + ". The plot of the film is: \n" +
+                response.data.Plot + " \n The film is acted by " + response.data.Actors + ".");
+            })
         .catch(function (error) {
             if (error.response) {
                 //copy pasted from a different .catch
@@ -94,4 +99,9 @@ let movieThis = function(movie){
 if (operator === "spotify-this-song") { spotifyThisSong(argument); }
 else if (operator === "concert-this") { concertThis(argument); }
 else if (operator === "movie-this") { movieThis(argument) }
-else if (operator === "do-what-it-says") { spotifyThisSong(); concertThis(); movieThis(); }
+else if (operator === "do-what-it-says") { 
+    console.log(variablesArray[0]);
+    spotifyThisSong(variablesArray[0]); 
+    concertThis(variablesArray[1]); 
+    movieThis(variablesArray[2]); 
+}
